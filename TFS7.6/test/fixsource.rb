cebb5727eab12c13f9b72917242c64a8a67178f7
DrewWeth <dgwetherington@gmail.com>
2015-07-08 20:22:22 -0500

    changed npc scripts

---
+++ TFS7.6/test/fixsource.rb
@@ -0,0 +1,29 @@
+require 'nokogiri'
+
+
+def correct_dir (dir)
+  return "data/npc/scripts/" + dir.split("/").last
+end
+
+def fix_files
+  files = Dir.entries(".")
+  files_count = files.count
+
+  files.each_with_index do |f_name, index|
+    if f_name.include? ".xml"
+      file = File.open(f_name)
+      doc = Nokogiri::XML(File.open(file))
+      npc = doc.at_css("npc")
+      script_string = npc["script"]
+      npc["script"] = correct_dir(script_string)
+
+      File.write(f_name, doc.to_xml)
+      puts "[#{index+1}/#{files_count}] #{npc["script"]} to #{f_name}"
+
+      file.close
+    end
+  end
+end
+
+
+fix_files
