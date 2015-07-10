7ab347a67c5887e189b019c4304be83a493880f1
DrewWeth <dgwetherington@gmail.com>
2015-07-10 14:25:10 -0500

    fixed boat npcs

---
+++ TFS7.6/data/npc/scripts/boat.lua
@@ -0,0 +1,12 @@
+<parameters>
+  <parameter key="module_travel" value="1"/>
+  <parameter key="message_greet" value="Hello |PLAYERNAME|. I can take you to Thais, Venore, Carlin, Port Hope, Ab'dendriel, Edron, Darashia, or Ankrahmun."/>
+  <parameter key="travel_destinations" value="venore,33210,31844,7,0;
+thais,33216,31844,7,0;
+carlin,32387,31821,6,0;
+port hope,32528,32784,6,0;
+ab'dendriel,32734,31669,6,0;
+edron,33175,31764,6,0;
+darashia,33289,32481,6,0;
+ankrahmun,33092,32884,6,0;"/>
+</parameters>
